e29db76ea78269616a21445d3af0bd89af0f8b52
Yann Jorelle <yannjorelle@proton.me>
2022-12-08 17:57:59 +0200

    enable refresh on message page

--- project3/ui/src/components/Message.jsx
+++ project3/ui/src/components/Message.jsx
@@ -1,40 +1,54 @@
 import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import messageService from "../services/messages";
 
-const Message = ({ message }) => {
+const Message = (props) => {
   const [reply, setReply] = useState("");
   const [replies, setReplies] = useState([]);
+  const [message, setMessage] = useState(props.message);
+  const { id } = useParams();
 
   useEffect(() => {
-    messageService.getReplies(message.id).then((res) => setReplies(res));
-  }, [message.id]);
+    if (!message) {
+      messageService.getById(id).then((m) => {
+        setMessage(m);
+        messageService.getReplies(m.id).then((res) => setReplies(res));
+      });
+    } else {
+      messageService.getReplies(message.id).then((res) => setReplies(res));
+    }
+  }, [message, id]);
 
   return (
     <div>
-      <h2>{message.text}</h2>
-      {replies.length > 0 && <h3>Replies</h3>}
-      <ul>
-        {replies.map((re) => (
-          <li key={re.id}>{re.text}</li>
-        ))}
-      </ul>
-      <h3>Add reply</h3>
-      <form
-        onSubmit={async (event) => {
-          event.preventDefault();
-          const newReply = await messageService.createReply(message.id, {
-            reply,
-          });
-          setReplies([newReply].concat(replies));
-          setReply("");
-        }}
-      >
-        <textarea
-          onChange={({ target }) => setReply(target.value)}
-          value={reply}
-        ></textarea>
-        <button type="submit">Submit</button>
-      </form>
+      {message && (
+        <div>
+          <h2>{message.text}</h2>
+          {replies.length > 0 && <h3>Replies</h3>}
+          <ul>
+            {replies.map((re) => (
+              <li key={re.id}>{re.text}</li>
+            ))}
+          </ul>
+          <h3>Add reply</h3>
+          <form
+            onSubmit={async (event) => {
+              event.preventDefault();
+              const newReply = await messageService.createReply(message.id, {
+                reply,
+              });
+              setReplies([newReply].concat(replies));
+              setReply("");
+            }}
+          >
+            <textarea
+              onChange={({ target }) => setReply(target.value)}
+              value={reply}
+            ></textarea>
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+      )}
     </div>
   );
 };
